e6f8a4b8e261bdc5c0d012bf4646e2198a697787
Sid ali dendenne <alilou@Alilou.168.1.2>
2025-09-26 17:28:44 +0100

    f

--- tool/listener.py
+++ tool/listener.py
@@ -7,13 +7,13 @@ def on_press(key):
         print(f'The special key {key} pressed')
 
 def on_release(key):
-    print(f'Key {key} released')
     if key == keyboard.Key.esc:
-        # Stop listener
+        print("Exiting...")
         return False
 
 # Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    while on_press:
+        print("Listening...")
+        
     listener.join()
